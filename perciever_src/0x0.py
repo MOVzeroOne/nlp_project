d146da3c8cc7876318d34614e1a9f468fd1e9d38
hal9000 <35937936+MOVzeroOne@users.noreply.github.com>
2021-04-24 23:22:52 +0200

    structure

---
+++ perciever_src/0x0.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt 
+import torch 
+import torch.nn as nn 
+import torch.optim as optim 
+import torch.nn.functional as F 
+import numpy as np 
+from tqdm import tqdm 
+
+
